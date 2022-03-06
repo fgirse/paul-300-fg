ea75d5c4b67bd15e03e949029640b428043ffefd
fgirse <fgirse@bluewin.ch>
2022-03-06 14:55:18 +0100

    initial commit9

--- components/Modal6.js
+++ components/Modal6.js
@@ -22,8 +22,10 @@ export default function MyModal5() {
           type="button"
           onClick={openModal}
           className=" w-max-lg text-xl px-4 lg:text-3xl lg:py-5 font-bold text-white bg-orange-600 hover:bg-slate-500 rounded-md bg-opacity-80 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-        >
-          <InformationCircleIcon className="w-8 h-6 fill-slate-50 inline" />
+        ><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="RGB(100,116,139)">
+        <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
+      </svg>
+         
           Reservationen
         </button>
       </div>
